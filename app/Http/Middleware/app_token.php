fcf4ac61348e876cc61321cf3bc23c6412344fb3
1735455665liu <1735455665@qq.com>
2019-05-16 11:12:56 +0800

    debug

--- app/Http/Middleware/app_token.php
+++ app/Http/Middleware/app_token.php
@@ -12,7 +12,7 @@ class app_token
     {
         $token=$request->input('token');
         $uid=$request->input('uid');
-        $key='laravelapp_token ';
+        $key='laravelapp_token';
         $redis_token=Redis::get($key);
         $userInfo=username::where(['id'=>$uid])->first();
         if($userInfo){
